736e3cbc1a74a1be078faa3bf39077667beaea9c
Kondwani David. Mwale <kondwanim@livestock.local>
2021-10-26 10:43:19 -0700

    recommit

--- models/Procurement.js
+++ models/Procurement.js
@@ -1,6 +1,8 @@
 const moment = require('moment');
 const mongoose = require('mongoose');
 const tz = require ('moment-timezone')
+const luxon = require ('luxon');
+const { DateTime } = require('luxon');
 
 
 const ProcurementSchema = new mongoose.Schema({
@@ -23,7 +25,8 @@ const ProcurementSchema = new mongoose.Schema({
         date: {         
             type:String,
           // default: moment().utc().format('dddd, MMMM Do YYYY, h:mm:ss a zz') 
-          default: moment().tz("Africa/Lusaka").format('dddd, MMMM Do YYYY') 
+         // default: moment().tz("Africa/Lusaka").format('dddd, MMMM Do YYYY') 
+          default: DateTime.now() 
           // default: moment().tz('America/Los_Angeles').format('dddd, MMMM Do YYYY, h:mm:ss a zz')         
         },
 
