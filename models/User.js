0af34669a6e8cae7361132329d5a36e982a736c1
Kondwani David. Mwale <kondwanim@livestock.local>
2021-11-03 16:59:42 -0700

    api changes

--- models/User.js
+++ models/User.js
@@ -1,12 +1,9 @@
+
 const mongoose = require('mongoose');
 const moment = require('moment');
-
 const tz = require ('moment-timezone');
 
-
-
-
-const Admin = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     name:{
         type: String,
         required: true,
@@ -25,149 +22,23 @@ const Admin = new mongoose.Schema({
         max:1024,
         min:8 
     },
-
-    date: {         
-        type:String,
-      // default: moment().utc().format('dddd, MMMM Do YYYY, h:mm:ss a zz') 
-      default: moment().tz("Africa/Lusaka").format('dddd, MMMM Do YYYY') 
-      
-      // default: moment().tz('America/Los_Angeles').format('dddd, MMMM Do YYYY, h:mm:ss a zz')         
-    },
-
-    role:{
-       
-            type: String,
-            default: 'is-admin'
-        
-        
-    }
-
-});
-
-
-const Procurement = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-        max: 255,
-        min: 2
-    },
-    email:{
-        type: String,
-        required: true,
-        max: 255,
-        min: 6
-    },
-    password:{
-        type: String,
-        required: true,
-        max:1024,
-        min:8 
-    },
-
-    date: {         
-        type:String,
-      // default: moment().utc().format('dddd, MMMM Do YYYY, h:mm:ss a zz') 
-      default: moment().tz("Africa/Lusaka").format('dddd, MMMM Do YYYY') 
-      
-      // default: moment().tz('America/Los_Angeles').format('dddd, MMMM Do YYYY, h:mm:ss a zz')         
-    },
-
-    role:{
-       
-        
-            type: String,
-            default: 'is-procurement'
-        
-        
-    }
-
-});
-
-
-const Compliance = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-        max: 255,
-        min: 2
-    },
-    email:{
-        type: String,
-        required: true,
-        max: 255,
-        min: 6
-    },
-    password:{
-        type: String,
-        required: true,
-        max:1024,
-        min:8 
-    },
-
-    date: {         
-        type:String,
-      // default: moment().utc().format('dddd, MMMM Do YYYY, h:mm:ss a zz') 
-      default: moment().tz("Africa/Lusaka").format('dddd, MMMM Do YYYY') 
-      
-      // default: moment().tz('America/Los_Angeles').format('dddd, MMMM Do YYYY, h:mm:ss a zz')         
-    },
-
-    role:{
-        
-       
-            type: String,
-            default: 'is-compliance'
-        
-        
-    }
-
-});
-
-
-const Finance = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-        max: 255,
-        min: 2
-    },
-    email:{
-        type: String,
-        required: true,
-        max: 255,
-        min: 6
-    },
-    password:{
-        type: String,
-        required: true,
-        max:1024,
-        min:8 
-    },
-
-    date: {         
-        type:String,
-      // default: moment().utc().format('dddd, MMMM Do YYYY, h:mm:ss a zz') 
-      default: moment().tz("Africa/Lusaka").format('dddd, MMMM Do YYYY') 
-      
-      // default: moment().tz('America/Los_Angeles').format('dddd, MMMM Do YYYY, h:mm:ss a zz')         
-    },
-
-    role:{
-       
-            type: String,
-            default: 'is-finance'
     
-    }
+    age:{
+        type: String,
+        required: true,
+        max:30,
+        min:3 ,
+       // default: "admin"
+        
+    },
 
+    date: {         
+        type:String,
+      // default: moment().utc().format('dddd, MMMM Do YYYY, h:mm:ss a zz') 
+      default: moment().tz("Africa/Lusaka").format('dddd, MMMM Do YYYY') 
+      
+      // default: moment().tz('America/Los_Angeles').format('dddd, MMMM Do YYYY, h:mm:ss a zz')         
+    },
 });
 
-
-
-const AdminSchema = mongoose.model("Administrators", Admin);
-const ProcurementSchema = mongoose.model("Procurement Dept Users", Procurement)
-const ComplianceSchema = mongoose.model("Compliance Dept Users", Compliance);
-const FinanceSchema = mongoose.model("Finance Dept Users", Finance)
-
-
-module.exports = { AUs: AdminSchema, PUs: ProcurementSchema, CUs:ComplianceSchema, FUs:FinanceSchema  }
+module.exports= mongoose.model('Users', UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 const { registerValidation, loginValidation } = require('../validation');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { AUs, CUs, FUs, PUs } = require('../models/User');
+
 
 router.get('/', async (req,res)=>{
   
@@ -15,9 +15,10 @@ router.get('/', async (req,res)=>{
 //GET ALL USERS
 router.get('/allUsers', async (req,res)=>{
     try {
-        const users = await User.find();
+        
+        const usersAdmin = await User.find();
         res.json({
-            message: users, 
+            message: usersAdmin, 
             status: 'Successfully retreived users!'
         });
  
@@ -26,132 +27,44 @@ router.get('/allUsers', async (req,res)=>{
     }
  });
 
-  //LOGIN Admin USERS
-    router.post('/login/admin',async (req,res)=>{
 
+ 
+  //LOGIN  USERS
+    router.post('/login',async (req,res)=>{
+
+
+      
+      const user = await User.findOne({ email: req.body.email });
+
+      console.log(user);
+
+      if(!user) return res.status(400).send('Email is not found');
+
+      const validPass = await bcrypt.compare(req.body.password, user.password);
+      if(!validPass) return res.status(400).send('Password is invalid');
+
+      
+
+
+      
+        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+        res.header('auth-token',token).send({ message: `Logged in as ${req.body.role} !`,
+          name:user.name, id: user._id, role: user.role, token: token});
+ 
+       res.send('Logged In ');
         
+
+
       const {error}= loginValidation(req.body);
       if (error) return res.status(400).send(error.details[0].message);
 
-      //CHECK IF EMAIL ALREADY EXISTS
-       const user = await AUs.findOne({ email: req.body.email });
-       if(!user) return res.status(400).send('Email is not found');
-      
-    //    //CHECK FOR NAME      
-    //     const username = await User.findOne({name: req.body.name });
-    //    if(!username) return res.status(400).send('Name is not found');
-
-      //CHECK IF USER PASSWORD MATCHES EMAIL
-    const validPass = await bcrypt.compare(req.body.password, user.password);
-    if(!validPass) return res.status(400).send('Password is invalid');
-        
-
-    //CREATE AND ASSIGN A TOKEN
-    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-    res.header('auth-token',token).send({ message: 'Logged In as Admin!', name:user.name, id: user._id, role: user.role, token: token});
-
-      res.send('Logged In as Admin!');
-    
-     
-        
+       
      
     });
 
 
-      //LOGIN Procurement USERS
-      router.post('/login/proc',async (req,res)=>{
-
-        
-        const {error}= loginValidation(req.body);
-        if (error) return res.status(400).send(error.details[0].message);
-  
-        //CHECK IF EMAIL ALREADY EXISTS
-         const user = await PUs.findOne({ email: req.body.email });
-         if(!user) return res.status(400).send('Email is not found');
-        
-      //    //CHECK FOR NAME      
-      //     const username = await User.findOne({name: req.body.name });
-      //    if(!username) return res.status(400).send('Name is not found');
-  
-        //CHECK IF USER PASSWORD MATCHES EMAIL
-      const validPass = await bcrypt.compare(req.body.password, user.password);
-      if(!validPass) return res.status(400).send('Password is invalid');
-          
-  
-      //CREATE AND ASSIGN A TOKEN
-      const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-      res.header('auth-token',token).send({ message: 'Logged In as Procurement!', name:user.name, id: user._id, role: user.role, token: token});
-  
-        res.send('Logged In Procurement!');
-      
-       
-          
-       
-      });
 
 
-        //LOGIN Compliance USERS
-    router.post('/login/comp',async (req,res)=>{
-
-        
-      const {error}= loginValidation(req.body);
-      if (error) return res.status(400).send(error.details[0].message);
-
-      //CHECK IF EMAIL ALREADY EXISTS
-       const user = await CUs.findOne({ email: req.body.email });
-       if(!user) return res.status(400).send('Email is not found');
-      
-    //    //CHECK FOR NAME      
-    //     const username = await User.findOne({name: req.body.name });
-    //    if(!username) return res.status(400).send('Name is not found');
-
-      //CHECK IF USER PASSWORD MATCHES EMAIL
-    const validPass = await bcrypt.compare(req.body.password, user.password);
-    if(!validPass) return res.status(400).send('Password is invalid');
-        
-
-    //CREATE AND ASSIGN A TOKEN
-    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-    res.header('auth-token',token).send({ message: 'Logged In as Compliance!', name:user.name, id: user._id, role:user.role, token: token});
-
-      res.send('Logged In Compliance!');
-    
-     
-        
-     
-    });
-
-
-      //LOGIN Finance USERS
-      router.post('/login/fin',async (req,res)=>{
-
-        
-        const {error}= loginValidation(req.body);
-        if (error) return res.status(400).send(error.details[0].message);
-  
-        //CHECK IF EMAIL ALREADY EXISTS
-         const user = await FUs.findOne({ email: req.body.email });
-         if(!user) return res.status(400).send('Email is not found');
-        
-      //    //CHECK FOR NAME      
-      //     const username = await User.findOne({name: req.body.name });
-      //    if(!username) return res.status(400).send('Name is not found');
-  
-        //CHECK IF USER PASSWORD MATCHES EMAIL
-      const validPass = await bcrypt.compare(req.body.password, user.password);
-      if(!validPass) return res.status(400).send('Password is invalid');
-          
-  
-      //CREATE AND ASSIGN A TOKEN
-      const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-      res.header('auth-token',token).send({ message: 'Logged In as Finance!', name:user.name, id: user._id, role:user.role, token: token});
-  
-        res.send('Logged In as Finance!');
-      
-       
-          
-       
-      });
 
     //LOGOUT USER
     router.get('/logout', async (req,res) => {
@@ -159,54 +72,17 @@ router.get('/allUsers', async (req,res)=>{
         res.redirect('/auth/login').send('Logged Out!');
     });
 
-    //REGISTER USER
+
+    
+
+      //-------------------------REGISTER USERS-----------------------------------------
+      //REGISTER USER
     router.post('/register',async (req,res)=>{
-        const {error}= registerValidation(req.body);
-       if (error) return res.status(400).send(error.details[0].message);
-
-    //CHECK IF EMAIL ALREADY EXISTS
-       const emailExist = await User.findOne({ email: req.body.email});
-    //HASH PASSWORD
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
-
-       if (emailExist) {
-           return res.status(400).send('Email already exists!')
-       } else {
-           
-         //CREATE NEW USERS
-          try {  
-         const registeredUser= new User({
-             name: req.body.name,
-             email: req.body.email,
-             password: hashPassword,
-             role: req.body.role.admin
-         });
-      
-         console.log(registeredUser);
-         
-        const savedUser = await registeredUser.save();
-          
-              res.json({
-                 
-                  status: 'Successfully Registered User!',
-                  message: savedUser
-              });
-          } catch (err) {
-               res.json({ message: err })
-          }
-        }
-      });
-
-
-
-      //REGISTER ADMINS
-    router.post('/admin/register',async (req,res)=>{
       const {error}= registerValidation(req.body);
      if (error) return res.status(400).send(error.details[0].message);
 
   //CHECK IF EMAIL ALREADY EXISTS
-     const emailExist = await AUs.findOne({ email: req.body.email});
+     const emailExist = await User.findOne({ email: req.body.email});
   //HASH PASSWORD
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(req.body.password, salt);
@@ -215,154 +91,29 @@ router.get('/allUsers', async (req,res)=>{
          return res.status(400).send('Email already exists!')
      } else {
          
-       //CREATE NEW ADMINS
+       //CREATE NEW USERS
         try {  
-       const registeredAdmin= new AUs({
+       const registeredUser= new User({
            name: req.body.name,
            email: req.body.email,
            password: hashPassword,
-           role: req.body.role
+           role: req.body.age
        });
     
-       console.log(registeredAdmin);
+       console.log(registeredUser);
        
-      const savedAdmin = await registeredAdmin.save();
+      const savedUser = await registeredUser.save();
         
             res.json({
                
-                status: 'Successfully Registered New Admin User!',
-                message: savedAdmin
+                status: 'Successfully Registered User!',
+                message: savedUser
             });
         } catch (err) {
              res.json({ message: err })
         }
       }
     });
-
-
-
-
-
-
-      //-------------------------REGISTER PROCUREMENT USERS
-      router.post('/procurement/register',async (req,res)=>{
-        const {error}= registerValidation(req.body);
-       if (error) return res.status(400).send(error.details[0].message);
-  
-    //CHECK IF EMAIL ALREADY EXISTS
-       const emailExist = await PUs.findOne({ email: req.body.email});
-    //HASH PASSWORD
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
-  
-       if (emailExist) {
-           return res.status(400).send('Email already exists!')
-       } else {
-           
-         //CREATE PROCUREMENT USERS
-          try {  
-         const registeredProc= new PUs({
-             name: req.body.name,
-             email: req.body.email,
-             password: hashPassword,
-             role: req.body.role
-         });
-      
-         console.log(registeredProc);
-         
-        const savedProc = await registeredProc.save();
-          
-              res.json({
-                 
-                  status: 'Successfully Registered New Procurment User!',
-                  message: savedProc
-              });
-          } catch (err) {
-               res.json({ message: err })
-          }
-        }
-      });
-
-
-      //-------------------------REGISTER COMPLIANCE USERS
-      router.post('/compliance/register',async (req,res)=>{
-        const {error}= registerValidation(req.body);
-       if (error) return res.status(400).send(error.details[0].message);
-  
-    //CHECK IF EMAIL ALREADY EXISTS
-       const emailExist = await CUs.findOne({ email: req.body.email});
-    //HASH PASSWORD
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
-  
-       if (emailExist) {
-           return res.status(400).send('Email already exists!')
-       } else {
-           
-         //CREATE COMPLIANCE USERS
-          try {  
-         const registeredComp= new CUs({
-             name: req.body.name,
-             email: req.body.email,
-             password: hashPassword,
-             role: req.body.role
-         });
-      
-         console.log(registeredComp);
-         
-        const savedComp = await registeredComp.save();
-          
-              res.json({
-                 
-                  status: 'Successfully Registered New Compliance User!',
-                  message: savedComp
-              });
-          } catch (err) {
-               res.json({ message: err })
-          }
-        }
-      });
-
-
-      //-------------------------REGISTER FINANCE USERS
-      router.post('/finance/register',async (req,res)=>{
-        const {error}= registerValidation(req.body);
-       if (error) return res.status(400).send(error.details[0].message);
-  
-    //CHECK IF EMAIL ALREADY EXISTS
-       const emailExist = await FUs.findOne({ email: req.body.email});
-    //HASH PASSWORD
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
-  
-       if (emailExist) {
-           return res.status(400).send('Email already exists!')
-       } else {
-           
-         //CREATE FINANCE USERS
-          try {  
-         const registeredFin= new FUs({
-             name: req.body.name,
-             email: req.body.email,
-             password: hashPassword,
-             role: req.body.role
-         });
-      
-         console.log(registeredFin);
-         
-        const savedFin = await registeredFin.save();
-          
-              res.json({
-                 
-                  status: 'Successfully Registered New Finance User!',
-                  message: savedFin
-              });
-          } catch (err) {
-               res.json({ message: err })
-          }
-        }
-      });
-  
   
   
 
