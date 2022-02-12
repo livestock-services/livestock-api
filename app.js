8bccd26fbe747b8a6a3c602d99bc170b54c5a4ca
Kondwani David. Mwale <kondwanim@livestock.local>
2022-02-12 10:04:25 +0200

    cors header

--- app.js
+++ app.js
@@ -23,10 +23,11 @@ app.use('/pfis', procurementRoute);
 app.use('/comp/permits', complianceRoute);
 app.use('/finance', financeRoute);
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-  };
-
+app.use(function (req, res, next){
+    res.header("Access-Control-Allow-Origin","*"); //or specify with your domain i.e http://localhost:3000
+    res.header("Access-Control-Allow-Origin","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 // Default Route
 app.get('/', (req,res) =>{
