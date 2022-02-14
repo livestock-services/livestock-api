60f718e846fa06fe899d9eaa1564d5b6b113bbca
Kondwani David. Mwale <kondwanim@livestock.local>
2022-02-14 15:46:23 +0200

    cors app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
- const cors = require('cors');
+
 
 // const corsOptions ={
 //     origin:'*', 
