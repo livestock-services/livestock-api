6775c85026de44e0ef0d8f7674b24a2328f4e102
Kondwani David. Mwale <kondwanim@livestock.local>
2022-02-15 15:56:11 +0200

    api rolled back changes

--- cors/cors.js
+++ cors/cors.js
@@ -2,10 +2,8 @@ const cors = require('cors');
 
 
  const corsOptions = {
-        origin:['http://localhost:3000', 'https://livestock-api.herokuapp.com'],
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        credentials:false,            //access-control-allow-credentials:true
-        optionSuccessStatus:200
+        origin:'*',
+       
      };
 
 module.exports = cors(corsOptions);
