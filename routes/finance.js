b6d616b1c30ea30ec064058bbaf5064b8b53543e
Kondwani David. Mwale <kondwanim@livestock.local>
2022-02-17 12:56:55 +0200

    headers

--- routes/finance.js
+++ routes/finance.js
@@ -13,6 +13,12 @@ router.get('/', async (req,res)=>{
 
 //GET ALL PFIs
 router.get('/allFinReports', async (req,res)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+    
     try {
         const allFinReports = await Finance.find();
         res.json({
