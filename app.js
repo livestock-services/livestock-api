2ea3d0ecd99f391d1a4043759a5d1a737ca4ab16
Kondwani David. Mwale <kondwanim@livestock.local>
2022-02-21 20:53:42 +0200

    cors headers

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ const financeRoute = require('./routes/finance');
 
 
 //----------middleware-------------------------
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
   });
 
 // app.use(cors({
